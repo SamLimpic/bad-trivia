16313a2b9b87fca7244a2cfd15024ece94fde2ec
Sam Limpic <samlimpic@gmail.com>
2021-04-05 17:40:32 -0600

    git friendly

--- app/Services/Axiosservice.js
+++ app/Services/Axiosservice.js
@@ -1,5 +1,5 @@
 // @ts-ignore
 export const api = axios.create({
-    baseURL: 'https://opentdb.com/api.php?amount=9&category=9&difficulty=easy&type=boolean',
+    baseURL: 'http://opentdb.com/api.php?amount=9&category=9&difficulty=easy&type=boolean',
     timeout: 30000
 })
