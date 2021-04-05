e6a074c457b184671134e204946b2cbc0010597b
Sam Limpic <samlimpic@gmail.com>
2021-04-05 17:44:24 -0600

    git fix?

--- app/Controllers/QuestionsController.js
+++ app/Controllers/QuestionsController.js
@@ -6,6 +6,7 @@ import { questionsService } from "../Services/QuestionsService.js";
 function _draw() {
   let questions = ProxyState.questions
   let template = ''
+
   questions.forEach(question => template += question.Template)
   document.getElementById("trivia").innerHTML = template
 }
